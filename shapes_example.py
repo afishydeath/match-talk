56c3ee1a4d686b6c8af26f540e35496335dd18c3
syn <afishydeath@gmail.com>
2025-09-11 00:45:07 +1000

    shape example update

--- shapes_example.py
+++ shapes_example.py
@@ -18,23 +18,41 @@ class Shape:
     def area(self) -> int | float:
         match self:
             case Circle():
-                return math.pi * self.r**2
-            case Rectangle(_, w, h):
-                return w * h
+                return math.pi * self.radius**2
+
             case Shape(side=s):
                 return s**2
+
+            case Rectangle(_, w, h):
+                return w * h
+
             case _:
                 raise Exception("Unhandled Shape!!")
 
-    def perimiter(self) -> int: ...
+    def contains_point(self, point: Point) -> bool:
+        match self:
+            case Circle(Point(x, y), r):
+                return (point.x - x) ** 2 + (point.y - y) ** 2 <= r**2
+
+            case Square(p, s):
+                return Rectangle(p, s, s).contains_point(point)
+
+            case Rectangle(Point(x, y), w, h) if (
+                point.x >= x and point.x <= x + w and point.y >= y and point.y <= y + h
+            ):
+                return True
+            case Rectangle():
+                return False
+            case _:
+                raise Exception("Unhandled Shape!")
 
 
 class Circle(Shape):
     __match_args__ = ("point", "radius")
 
-    def __init__(self, p: Point, r: int) -> None:
+    def __init__(self, p: Point, r: int | float) -> None:
         super().__init__(p)
-        self.r: int | float = r
+        self.radius: int | float = r
 
 
 class Rectangle(Shape):
@@ -55,8 +73,9 @@ class Square(Rectangle):
 
 
 if __name__ == "__main__":
-    c = Circle(Point(0, 0), 1)
-    r = Rectangle(Point(1, 1), 2, 4)
-    s = Square(Point(3, 5), 6)
-    for i in (c, r, s):
-        print(f"{i.area()=}")
+    testc = Circle(Point(0, 0), 2)
+    testr = Rectangle(Point(1, 1), 2, 4)
+    tests = Square(Point(3, 5), 6)
+    testp = Point(1, 1)
+    for i in (testc, testr, tests):
+        print(f"{i.area()=} {i.contains_point(testp)=}")
